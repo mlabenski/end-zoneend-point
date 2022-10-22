54c6b1b348df6f2edc967f4ba8dfb9afd1e25373
Mitchell <mlabenski123@gmail.com>
2022-10-22 14:36:01 -0700

    reddis big working plz

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ def refresh():
             "price": 'total_amount',
             "order_details": order_details
         }
-        db.hset("order_id", woo_id, datadict)
+        db.hset("order_id", woo_id, jsonify(datadict))
         return "https://cpswoo.securepayments.cardpointe.com/pay?total="+price+"&cf_hidden_woo_id="+woo_id+"&details="+order_details, 200
 
         # jsonData = json.dumps(params)
