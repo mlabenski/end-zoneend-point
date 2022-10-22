1d1199b61c25adc30b27d6da60a0952f8f376949
Mitchell <mlabenski123@gmail.com>
2022-10-22 12:07:02 -0700

    request.getdata

--- app/main.py
+++ app/main.py
@@ -7,8 +7,11 @@ app = Flask(__name__)
 @app.route('/data', methods=['GET', 'POST'])
 def refresh():
     if request.method == 'POST':
-        data = request.get_data();
-        print(data);
+        woo_id = request.form['woo_id'];
+        price = request.form['total_amount']
+        order_details = request.form['order']
+
+        print(woo_id, price, order_details);
         return "https://cpswoo.securepayments.cardpointe.com/pay?total=25.00&cf_hidden_woo_id=72&details=BEASySpecialtybathroom%7C1%7C25", 200
 
         # jsonData = json.dumps(params)
