ecd6b7ed2493c1308e25e623d68bbb259b9e1c67
Mitchell <mlabenski123@gmail.com>
2022-10-22 12:34:00 -0700

    retrieve data from post and build link complete

--- app/main.py
+++ app/main.py
@@ -3,14 +3,23 @@ from flask_redis import FlaskRedis
 
 
 app = Flask(__name__)
-
+# README: where is the best spot to include redis db calls?
+# A separated interface?
+redis_client = FlaskRedis(app)
 @app.route('/data', methods=['GET', 'POST'])
 def refresh():
     if request.method == 'POST':
-        woo_id = request.form['woo_id'];
+        woo_id = request.form['woo_id']
         price = request.form['total_amount']
         order_details = request.form['order']
+        datadict = {
+            "woo_id": woo_id,
+            "price": 'total_amount',
+            "order_details": order_details
+        }
 
+        redis_client.set('orderID', woo_id)
+        redis_client.hmset("orders_" + woo_id, datadict)
         return "https://cpswoo.securepayments.cardpointe.com/pay?total="+price+"&cf_hidden_woo_id="+woo_id+"&details="+order_details, 200
 
         # jsonData = json.dumps(params)
