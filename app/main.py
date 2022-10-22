c64a643b6fb66e57bcd3af367e6be0ae956749bf
Mitchell <mlabenski123@gmail.com>
2022-10-22 15:16:50 -0700

    reddis big working plz

--- app/main.py
+++ app/main.py
@@ -18,10 +18,11 @@ def refresh():
     if request.method == 'POST':
         woo_id = request.form['woo_id'],
         price = request.form['total_amount'],
-        order_details = request.form['order']
+        order_details = request.form['order_id']
         string_data = "woo_id"+str(woo_id)+"price="+str(price)+"order="+str(order_details)
+        print(string_data)
         db.set("order", string_data)
-        return "https://cpswoo.securepayments.cardpointe.com/pay?total="+price+"&cf_hidden_woo_id="+woo_id+"&details="+order_details, 200
+        return "https://cpswoo.securepayments.cardpointe.com/pay?total="+str(price)+"&cf_hidden_woo_id="+str(woo_id)+"&details="+str(order_details), 200
 
         # jsonData = json.dumps(params)
         # jsonData should be transferred to redis server.
