c8c33c6e9e2ffe86c668bce77d92cf78143cc3e8
Mitchell <mlabenski123@gmail.com>
2022-10-22 12:18:25 -0700

    retrieve data from post and build link complete

--- app/main.py
+++ app/main.py
@@ -11,8 +11,7 @@ def refresh():
         price = request.form['total_amount']
         order_details = request.form['order']
 
-        print(woo_id, price, order_details);
-        return "https://cpswoo.securepayments.cardpointe.com/pay?total=25.00&cf_hidden_woo_id=72&details=BEASySpecialtybathroom%7C1%7C25", 200
+        return "https://cpswoo.securepayments.cardpointe.com/pay?total="+price+"&cf_hidden_woo_id="+woo_id+"&details="+order_details, 200
 
         # jsonData = json.dumps(params)
         # jsonData should be transferred to redis server.
